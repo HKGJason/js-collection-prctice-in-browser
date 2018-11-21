fdb7a4ad3d437ebffc001e181fcb373946df4af6
Jason Liu <jason.liu@oocl.com>
2018-11-21 11:42:31 +0800

    refunction pracitce 1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -3,12 +3,6 @@
 
 function collectSameElements(collectionA, collectionB) {
   //return 'Implement the practice require, and begin changing code in this row';
-  var common = [];
-    for ( var i =0;i<collectionA.length;i++){
-		for ( var j = 0; j<collectionB.length;j++){
-			if (collectionA[i] === collectionB [j])
-				common.push(collectionA[i]);
-		}
-	}
-	return common;
+ 
+  return collectionA.filter(a => collectionB.includes(a));
 }
