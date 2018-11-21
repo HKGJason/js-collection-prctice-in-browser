40c6f77ebdde6ddb284345b3778bf268cee00692
Jason Liu <jason.liu@oocl.com>
2018-11-21 11:53:26 +0800

    refunction practice 4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,14 +1,6 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-    var common = [];
-	
-  var arrB = objectB.value;
-    for ( var i =0;i<collectionA.length;i++){
-		for ( var j = 0; j<arrB.length;j++){
-			if (collectionA[i].key === arrB [j])
-				common.push(collectionA[i].key);
-		}
-	}
-	return common;
+	const result = collectionA.filter(a=> objectB.value.includes(a.key));
+	return result.map(arr => arr.key);
 }
